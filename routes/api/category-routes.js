f49b1b45c706b5ced84ee09acb7d58a871c32b4b
jmcshan1 <jmcshan01@gmail.com>
2023-09-19 20:28:25 -0500

    added to category and product routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -48,10 +48,10 @@ router.put("/:id", (req, res) => {
 
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
   try {
-    const categoryData = await Category.destroy({
+    const categoryData =  Category.destroy({
       where: {
         id: req.params.id,
       },
